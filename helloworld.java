355940fcbcaba1a957f5901decaf2484ac32a5d7
vasusakthi008-max <vasusakthi008@gmail.com>
2026-03-26 13:26:52 +0530

    created HELLOWORLD

---
+++ helloworld.java
@@ -0,0 +1,28 @@
+// UC5 - Display Hello using Enhanced For Loop
+
+public class helloworld {
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+
+            System.out.print("Hello ");
+
+            int count = 0;
+
+            for (String name : args) {
+                System.out.print(name);
+
+                count++;
+                if (count < args.length) {
+                    System.out.print(", ");
+                }
+            }
+
+            System.out.println();
+
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
