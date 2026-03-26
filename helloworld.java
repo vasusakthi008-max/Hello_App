96cd3ed38360c006f958f60136e3472e5c82dfad
vasusakthi008-max <vasusakthi008@gmail.com>
2026-03-26 13:36:53 +0530

    Update helloworld.java
    code the fie

--- helloworld.java
+++ helloworld.java
@@ -1,25 +1,19 @@
-// UC5 - Display Hello using Enhanced For Loop
-
-public class helloworld {
+public class HelloApp {
 
     public static void main(String[] args) {
 
         if (args.length > 0) {
 
-            System.out.print("Hello ");
-
-            int count = 0;
+            String names = "";
 
             for (String name : args) {
-                System.out.print(name);
-
-                count++;
-                if (count < args.length) {
-                    System.out.print(", ");
-                }
+                names = names + name + ", ";
             }
 
-            System.out.println();
+            // Remove last ", "
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello " + names);
 
         } else {
             System.out.println("Hello World");
